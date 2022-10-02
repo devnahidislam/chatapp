33e5207cda7c8c1a60cf3133e78290ff799405d9
mdnahidislam <mdnahidislam30@gmail.com>
2022-10-02 10:40:27 +0600

    Get user, follow & undollow user function created.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -29,3 +29,59 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    const { password, updatedAt, ...others } = user._doc;
+
+    res.status(200).send(others);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const follow = async (req, res, next) => {
+  if (req.params.fid !== req.user.id) {
+    try {
+      const user = await User.findById(req.params.fid);
+      const currentUser = await User.findById(req.user.id);
+      if (!user.followers.includes(req.user.id)) {
+        await user.updateOne({ $push: { followers: req.user.id } });
+        await currentUser.updateOne({
+          $push: { following: req.params.fid },
+        });
+        res.status(200).send(`You are following ${user.username}`);
+      } else {
+        res.status(403).send(`You are already following ${user.username}`);
+      }
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    res.status(403).send("You can't follow yourself.");
+  }
+};
+
+export const unFollow = async (req, res, next) => {
+  if (req.params.ufid !== req.user.id) {
+    try {
+      const user = await User.findById(req.params.ufid);
+      const currentUser = await User.findById(req.user.id);
+      if (user.followers.includes(req.user.id)) {
+        await user.updateOne({ $pull: { followers: req.user.id } });
+        await currentUser.updateOne({
+          $pull: { following: req.params.ufid },
+        });
+        res.status(200).send(`You unfollowed ${user.username}`);
+      } else {
+        res.status(403).send(`You didn't follow ${user.username}`);
+      }
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    res.status(403).send("You can't unfollow yourself");
+  }
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,22 @@
 import express from 'express';
-import { deleteUser, update } from '../controllers/user.js';
+import { deleteUser, follow, getUser, unFollow, update } from '../controllers/user.js';
 import { verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 // Update
 router.put('/:id', verifyUser, update);
+
 // Delete
 router.delete('/:id', verifyUser, deleteUser);
+
 // Get Users
-router.get('/', (req, res) => {
-  res.send('Users Route');
-});
+router.get('/:id', verifyUser, getUser);
+
 // Follow
+router.put('/:id/:fid/follow', verifyUser, follow);
+
 // Unfollow
+router.put('/:id/:ufid/unfollow', verifyUser, unFollow);
+
 export default router;
