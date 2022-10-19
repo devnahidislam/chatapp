8afc77f0bc35f6edf495ae83e79e66d5554674ae
mdnahidislam <mdnahidislam30@gmail.com>
2022-10-19 18:58:23 +0600

    Changed routes.

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,16 @@ import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 import Register from './pages/auth/Register';
 import Login from './pages/auth/Login';
-import { useContext } from 'react';
-import { AuthContext } from './context/AuthContext';
 
 function App() {
-  const { user } = useContext(AuthContext);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={user ? <Home /> : <Login />} />
-          <Route path="/register" element={user ? <Home /> : <Register />} />
-          <Route path="/login" element={user ? <Home /> : <Login />} />
+          <Route index element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
